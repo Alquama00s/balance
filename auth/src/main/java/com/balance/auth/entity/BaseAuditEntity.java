4ca2821220fd995ab366086b4258f98b3ed5e025
Alquama <alquamasalim000@gmail.com>
2026-04-26 11:48:33 +0530

    liquibase

--- auth/src/main/java/com/balance/auth/entity/BaseAuditEntity.java
+++ auth/src/main/java/com/balance/auth/entity/BaseAuditEntity.java
@@ -3,6 +3,9 @@ package com.balance.auth.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -12,9 +15,11 @@ import java.util.UUID;
 public abstract class BaseAuditEntity {
 
     @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
     private OffsetDateTime createdAt;
 
     @Column(name = "updated_at")
+    @UpdateTimestamp
     private OffsetDateTime updatedAt;
 
     @Column(name = "deleted_at")
